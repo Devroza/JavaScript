aeed8608889fea38ecf2974e6e76e3609077d9c4
Mahavatar Sadhu-rishi de Roza <124314801+Devroza@users.noreply.github.com>
2023-07-19 13:00:53 -0300

    Criação dos Exercícios sobre Condições JS

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 72.8
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //Condição Simples, pois não possui "else"
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Dirija sempre usando cinto de segurança`)
+
