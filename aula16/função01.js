ecd6967f0187f04a1e9ba50fc4d061db40ef848b
Mahavatar Sadhu-rishi de Roza <124314801+Devroza@users.noreply.github.com>
2023-11-14 18:05:41 -0300

    Exercícios sobre function (aula 16)

---
+++ aula16/função01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar'
+    }
+}
+
+//Modo alternativo de mostrar no console:
+//Console.log(parimpar(223))
+
+var res = parimpar(4)
+console.log(res)
---
+++ aula16/função02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { //Posso adicionar um "=0" na frente dos elementos para atribui-los um valor caso o mesmo não seja definido, no caso se eu não definir o valor, ele será "0".
+    return n1 + n2
+}
+
+console.log(soma(7))
